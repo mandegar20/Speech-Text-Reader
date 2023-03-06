4c347424e2e4eff3660a2cecce7e266fdee5271a
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2023-03-06 18:27:19 +0430

    selection

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.querySelector("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
